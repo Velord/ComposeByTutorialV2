7e37f285f755a01c612e8734340b8d8aa510bac4
Velord <vladik.yaroshenko@yandex.ru>
2021-12-25 21:57:09 +0200

    init

---
+++ app/src/main/java/com/example/composebytutorialv2/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.composebytutorialv2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composebytutorialv2.ui.theme.ComposeByTutorialV2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DefaultPreview()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeByTutorialV2Theme {
+        Surface(color = MaterialTheme.colors.background) {
+            Greeting("Compose is a Shit")
+        }
+    }
+}
